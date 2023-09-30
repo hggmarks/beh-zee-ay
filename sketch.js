74d09ba4dbd31cc435670553189da8d1784bd877
Hugo Marques <43884516+hggmarks@users.noreply.github.com>
2023-09-30 00:40:41 -0300

    feat: multiple curves, point and curve deletion, hide, evaluation and more

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let clickControl = false;
 const pointSize = 15;
-const lineSize = 5;
+const lineSize = 2;
+const dotLineSize = 5;
 
 function setup() {
   createCanvas(windowWidth - 100, windowHeight);
@@ -13,28 +14,46 @@ function setup() {
 function draw() {
   background(0);
 
-  // Draw control points
-  for (let p of controlPoints) {
-    strokeWeight(pointSize);
-    point(p.x, p.y, 10, 10);
-    strokeWeight(lineSize);
-  }
-
-  // Calculate Bezier curve points
-  curvePoints = [];
+  //Draw poligonals
   
-  if (controlPoints.length >= 1 ) {
-    for (let t = 0; t <= 1.0001; t += 0.01) {
-      let p = deCasteljau(controlPoints, t);
-      curvePoints.push(createVector(p.x, p.y));
-    }
 
-    // Draw the Bezier curve
-    beginShape();
-    for (let p of curvePoints) {
-      vertex(p.x, p.y);
+  // Calculate and draw Bezier curve points
+  if (curves.length >= 1) {
+    stroke(0, 255, 0);
+    for (let curve of curves) {
+      if (curve.length >= 1 ) {
+        beginShape();
+        for (let t = 0; t <= 1.0001; t += 1/config['curveEvals']) {
+          let p = deCasteljau(curve, t);
+          if (state & 0b010) vertex(p.x, p.y);
+          //curvePoints.push(createVector(p.x, p.y));
+        }
+        endShape();
+      }
     }
-    endShape();
+    stroke(255);
+  }
+  // Draw control points
+  for(let curve of curves) {
+    for (let i=0; i<curve.length; ++i) {
+      p = curve[i];
+      if (i != curve.length - 1 && state & 0b100) {
+        nextP = curve[i+1];
+        strokeWeight(dotLineSize);
+        for (let t=0; t<=1; t += 0.08) {
+          const a = lerp(p.x, nextP.x, t);
+          const b = lerp(p.y, nextP.y, t);
+          point(a, b);
+        }
+        strokeWeight(lineSize);
+      }
+      if (curve == curves[currentCurve]) stroke(255, 0 ,0);
+      if (p == selectedPoint) stroke(255, 255, 0);
+      strokeWeight(pointSize);
+      if (state & 0b001) point(p.x, p.y, 10);
+      strokeWeight(lineSize);
+      stroke(255);
+    }
   }
 }
 
@@ -45,7 +64,7 @@ function windowResized() {
 function mousePressed() {
 
   if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
-    if (state & 0b1) {
+    if (shift) {
       addPoint();
     } else {
       selectedPoint = isControlPoint(mouseX, mouseY, pointSize); 
@@ -60,5 +79,15 @@ function mouseDragged() {
 
 
 function mouseReleased() {
-  onMouseReleased();
+  //onMouseReleased();
+}
+
+
+function keyPressed() {
+  handleKeyPressed();
+}
+
+
+function keyReleased() {
+  handleKeyReleased();
 }
